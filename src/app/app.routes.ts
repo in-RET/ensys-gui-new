c391ef46ce591b4daaffc64cc14aa15593d68c4a
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2025-01-15 16:58:08 +0100

    feat: content+improvements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./core/layout/content/content.component')
-            .then(c => c.ContentComponent),
-    },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./core/layout/content/content.component').then(
+        (c) => c.ContentComponent
+      ),
+  },
 
-    {
-        path: '**',
-        redirectTo: '',
-    },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
--- src/app/core/layout/content/content.component.ts
+++ src/app/core/layout/content/content.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { IndexComponent } from '../index/index.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-content',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, IndexComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss',
   standalone: true,
