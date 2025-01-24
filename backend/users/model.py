7b3389d760b70e75c2431ebfd42b5e69e9f0232a
Andreas Lubojanski <andreas@lubojanski.com>
2025-01-24 18:24:41 +0100

    (feat) added jwt token support

--- backend/users/model.py
+++ backend/users/model.py
@@ -1,13 +1,22 @@
-from pydantic import BaseModel
+from datetime import datetime
+
+from passlib.hash import pbkdf2_sha256
+from sqlmodel import Field, SQLModel
 from typing import Optional
-from uuid import UUID, uuid4
 
-
-class EnsysUser(BaseModel):
-    id: Optional[UUID] = uuid4()
-    name: str
-    surname: str
+class EnsysUser(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    firstname: str
+    lastname: str
+    password: str
     mail: str
-    is_active: bool
-    is_superuser: Optional[bool] = False
-    is_staff: Optional[bool] = False
+    is_active: bool = Field(default=False)
+    is_superuser: bool = Field(default=False)
+    is_staff: bool = Field(default=False)
+    last_login: Optional[datetime] = Field(default=None, nullable=True)
+    date_joined: Optional[datetime] = Field(default=None, nullable=True)
+
+    def verify_password(self, plain_password: str) -> bool:
+        return pbkdf2_sha256.verify(plain_password, self.password)
+
