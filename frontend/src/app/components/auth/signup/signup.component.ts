5b90d3561c6203fea887363708fe2e8caf99eaa4
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2025-02-27 14:39:59 +0100

    feat(auth): validation on signup form

--- frontend/src/app/components/auth/signup/signup.component.ts
+++ frontend/src/app/components/auth/signup/signup.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+    FormControl,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { ValidateService } from '../services/validate.service';
 
 @Component({
-  selector: 'app-signup',
-  imports: [],
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+    selector: 'app-signup',
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    templateUrl: './signup.component.html',
+    styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+    validateService = inject(ValidateService);
 
+    form: FormGroup = new FormGroup(
+        {
+            email: new FormControl(null, Validators.required),
+            fName: new FormControl(null, Validators.required),
+            lName: new FormControl(null, Validators.required),
+            user: new FormControl(null, Validators.required),
+            pass: new FormControl(null, [
+                Validators.required,
+                this.validateService.passwordMinLowerCaseLettersValidator(),
+            ]),
+            confirmPass: new FormControl(
+                null,
+                Validators.compose([Validators.required])
+            ),
+            consentOpt: new FormControl(false, Validators.required),
+        },
+        this.validateService.passwordMatch('pass', 'confirmPass')
+    );
+
+    get email() {
+        return this.form.get('email');
+    }
+
+    get fName() {
+        return this.form.get('fName');
+    }
+
+    get lName() {
+        return this.form.get('lName');
+    }
+
+    get user() {
+        return this.form.get('user');
+    }
+
+    get pass() {
+        return this.form.get('pass');
+    }
+
+    get confirmPass() {
+        return this.form.get('confirmPass');
+    }
+
+    get consentOpt() {
+        return this.form.get('consentOpt');
+    }
+
+    signup() {}
 }
