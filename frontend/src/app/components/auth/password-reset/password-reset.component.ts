3647e6ddb076fa2f93ac3ee0ee8bd680ca54481a
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2025-02-27 14:50:00 +0100

    feat(auth): validation on reset password form

--- frontend/src/app/components/auth/password-reset/password-reset.component.ts
+++ frontend/src/app/components/auth/password-reset/password-reset.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {
+    FormControl,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-password-reset',
-  imports: [],
-  templateUrl: './password-reset.component.html',
-  styleUrl: './password-reset.component.scss'
+    selector: 'app-password-reset',
+    imports: [FormsModule, ReactiveFormsModule],
+    templateUrl: './password-reset.component.html',
+    styleUrl: './password-reset.component.scss',
 })
 export class PasswordResetComponent {
-
+    email: FormControl = new FormControl(null, Validators.required);
 }
