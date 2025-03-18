540a2bbcbb3ba8aaed1256de9f0a4873d12ae3bd
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2025-03-18 03:22:49 +0100

    feat(project): create project template

--- frontend/src/app/components/project/project-create/project-create.component.ts
+++ frontend/src/app/components/project/project-create/project-create.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+    FormControl,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-project-create',
-  imports: [],
-  templateUrl: './project-create.component.html',
-  styleUrl: './project-create.component.scss'
+    selector: 'app-project-create',
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
+    templateUrl: './project-create.component.html',
+    styleUrl: './project-create.component.scss',
 })
 export class ProjectCreateComponent {
-
+    form: FormGroup = new FormGroup({
+        name: new FormControl(null, []),
+        description: new FormControl(null, []),
+        country: new FormControl('', []),
+        latitude: new FormControl('Click on the map', []),
+        longitude: new FormControl('Click on the map', []),
+        currency: new FormControl('', []),
+        unit_choice: new FormControl('', []),
+        unit_choice_co2: new FormControl('', []),
+    });
 }
--- frontend/src/app/components/project/project-explore/project-explore.component.ts
+++ frontend/src/app/components/project/project-explore/project-explore.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { ProjectService } from '../services/project.service';
 
 @Component({
     selector: 'app-project-explore',
-    imports: [CommonModule, RouterLink, RouterOutlet],
+    imports: [CommonModule, RouterLink],
     templateUrl: './project-explore.component.html',
     styleUrl: './project-explore.component.scss',
 })
