75f2e533eadc5dd92198fdb90c11a7dba33aab2d
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2025-03-26 15:37:04 +0100

    feat(scenario): setup step

--- frontend/src/app/components/scenario/scenario-base/scenario-base.component.ts
+++ frontend/src/app/components/scenario/scenario-base/scenario-base.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ScenarioSetupComponent } from '../scenario-setup/scenario-setup.component';
 import { ScenarioFooterComponent } from './scenario-footer/scenario-footer.component';
 import { ScenarioProgressionComponent } from './scenario-progression/scenario-progression.component';
 
 @Component({
     selector: 'app-scenario-base',
-    imports: [ScenarioProgressionComponent, ScenarioFooterComponent],
+    imports: [
+        CommonModule,
+        ScenarioProgressionComponent,
+        ScenarioFooterComponent,
+        ScenarioSetupComponent,
+    ],
     templateUrl: './scenario-base.component.html',
     styleUrl: './scenario-base.component.scss',
 })
--- frontend/src/app/components/scenario/scenario-setup/scenario-setup.component.ts
+++ frontend/src/app/components/scenario/scenario-setup/scenario-setup.component.ts
@@ -1,11 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+    FormControl,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-scenario-setup',
-  imports: [],
-  templateUrl: './scenario-setup.component.html',
-  styleUrl: './scenario-setup.component.scss'
+    selector: 'app-scenario-setup',
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    templateUrl: './scenario-setup.component.html',
+    styleUrl: './scenario-setup.component.scss',
 })
 export class ScenarioSetupComponent {
+    form: FormGroup = new FormGroup({
+        name: new FormControl(null, [Validators.required]),
+        simulationPeriod: new FormControl('', [Validators.required]),
+        timeStep: new FormControl(null, [Validators.required]),
+        sDate: new FormControl(null, [Validators.required]),
+        interestRate: new FormControl(null, [Validators.required]),
+        userMode: new FormControl('', [Validators.required]),
+        simulationYear: new FormControl('', [Validators.required]),
+    });
 
+    get name() {
+        return this.form.get('name');
+    }
+
+    get simulationPeriod() {
+        return this.form.get('simulationPeriod');
+    }
+
+    get timeStep() {
+        return this.form.get('timeStep');
+    }
+
+    get sDate() {
+        return this.form.get('sDate');
+    }
+
+    get interestRate() {
+        return this.form.get('interestRate');
+    }
+
+    get userMode() {
+        return this.form.get('userMode');
+    }
+
+    get simulationYear() {
+        return this.form.get('simulationYear');
+    }
 }
