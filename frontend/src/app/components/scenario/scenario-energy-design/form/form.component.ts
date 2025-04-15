2ebaa7b640fc0e024ac216b0f31ab8d173e4bd91
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2025-04-15 10:02:30 +0200

    feat(form): dynamic form+validation

---
+++ frontend/src/app/components/scenario/scenario-energy-design/form/form.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import {
+    FormControl,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+
+@Component({
+    selector: 'app-form',
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    templateUrl: './form.component.html',
+    styleUrl: './form.component.scss',
+})
+export class FormComponent {
+    form!: FormGroup;
+
+    @Input() formData!: any;
+    // @Input() form!: FormGroup;
+    // form = model<FormGroup>();
+
+    // get fControl()
+
+    ngOnInit() {
+        this.initForm();
+    }
+
+    initForm() {
+        this.form = new FormGroup({});
+
+        this.formData.sections.forEach((section: any) => {
+            section.fields.forEach((field: any) => {
+                let fControl: FormControl = new FormControl('', []);
+
+                if (field.isReq) fControl.addValidators(Validators.required);
+
+                this.form.addControl(field.name, fControl);
+            });
+        });
+    }
+}
--- frontend/src/app/components/scenario/scenario-energy-design/modal/modal.component.ts
+++ frontend/src/app/components/scenario/scenario-energy-design/modal/modal.component.ts
@@ -1,46 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
-import {
-    FormControl,
-    FormGroup,
-    FormsModule,
-    ReactiveFormsModule,
-    Validators,
-} from '@angular/forms';
 import * as bootstrap from 'bootstrap';
+import { FormComponent } from '../form/form.component';
 import { OrderListComponent } from '../order-list/order-list.component';
 
 @Component({
     selector: 'app-modal',
-    imports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        OrderListComponent,
-    ],
+    imports: [CommonModule, OrderListComponent, FormComponent],
     templateUrl: './modal.component.html',
     styleUrl: './modal.component.scss',
 })
 export class ModalComponent {
-    form!: FormGroup;
-
-    get portImportTotal() {
-        return this.form.get('portImportTotal');
-    }
-
-    get portExportTotal() {
-        return this.form.get('portExportTotal');
-    }
-
-    @ViewChild('modal') modalRef = {} as ElementRef;
-
-    @Input() title!: string;
-
+    formData!: any;
     inputPortList!: any[];
     outputPortList!: any[];
 
+    // temp
+    isFormValid: boolean = true;
+
+    @ViewChild('modal') modalRef = {} as ElementRef;
+    @ViewChild(FormComponent) formComponent!: FormComponent;
+
+    @Input() node!: any;
+    @Input() title!: string;
+
     ngOnInit() {
-        this.initForm();
+        this.initFormData(this.node.name);
 
         setTimeout(() => {
             const modal = new bootstrap.Modal(this.modalRef.nativeElement);
@@ -48,10 +33,162 @@ export class ModalComponent {
         }, 0);
     }
 
-    initForm() {
-        this.form = new FormGroup({
-            portImportTotal: new FormControl('', [Validators.required]),
-            portExportTotal: new FormControl('', [Validators.required]),
-        });
+    initFormData(nodeName: string) {
+        // this.form = signal<FormGroup>();
+
+        switch (nodeName) {
+            case 'Source':
+                this.formData = {
+                    sections: [
+                        {
+                            name: 'Name',
+                            class: 'col-12',
+                            fields: [
+                                {
+                                    name: 'name',
+                                    placeholder: 'Name',
+                                    label: 'Name',
+                                    isReq: true,
+                                    type: 'text',
+                                },
+                            ],
+                        },
+                        {
+                            name: 'Economical parameters',
+                            class: 'col-6',
+                            fields: [
+                                {
+                                    name: 'variable_costs',
+                                    placeholder: 'Variable costs',
+                                    label: 'Variable costs',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'maximum',
+                                    placeholder: 'Maximum',
+                                    label: 'Maximum',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'minimum',
+                                    placeholder: 'Minimum',
+                                    label: 'Minimum',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'existing',
+                                    placeholder: 'Existing',
+                                    label: 'Existing',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'capex',
+                                    placeholder: 'Capex',
+                                    label: 'Capex',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'opex',
+                                    placeholder: 'Opex',
+                                    label: 'Opex',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'offset',
+                                    placeholder: 'Offset',
+                                    label: 'Offset',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'lifetime',
+                                    placeholder: 'Lifetime',
+                                    label: 'Lifetime',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                            ],
+                        },
+
+                        {
+                            name: 'Technical parameters',
+                            class: 'col-6',
+                            fields: [
+                                {
+                                    name: 'Nominal_value',
+                                    placeholder: 'Nominal value',
+                                    label: 'Nominal value',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'max',
+                                    placeholder: 'max',
+                                    label: 'max',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'min',
+                                    placeholder: 'min',
+                                    label: 'min',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'Nonconvex',
+                                    placeholder: 'Nonconvex',
+                                    label: 'Nonconvex',
+                                    isReq: true,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'Summed_max',
+                                    placeholder: 'Summed max',
+                                    label: 'Summed max',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'Summed_min',
+                                    placeholder: 'Summed min',
+                                    label: 'Summed min',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'Emission_factor',
+                                    placeholder: 'Emission factor',
+                                    label: 'Emission factor',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                                {
+                                    name: 'Renewable_factor',
+                                    placeholder: 'Renewable factor',
+                                    label: 'Renewable factor',
+                                    isReq: false,
+                                    type: 'number',
+                                },
+                            ],
+                        },
+                    ],
+                };
+                break;
+        }
+    }
+
+    submit() {
+        if (this.formComponent.form.valid) {
+            this.isFormValid = true;
+            const formValue = this.formComponent.form.getRawValue();
+            console.log(formValue);
+        } else this.isFormValid = false;
     }
 }
