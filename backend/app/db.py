a7e9c75635d6bc5e42dd4451d64413c666e19f76
Andreas Lubojanski <andreas@lubojanski.com>
2025-05-16 14:09:49 +0200

    (fix) fixed broken docker compose and added celery support

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,21 +2,12 @@
 # database settings
 ########################################################################
 import os
-from enum import Enum
 
 from sqlalchemy import create_engine
-from sqlmodel import Session, SQLModel
+from sqlmodel import Session
 
-db_link = os.getenv("DATABASE_URL")
-db_engine = create_engine(db_link)
+db_engine = create_engine(os.getenv("DATABASE_URL"))
 
 def get_db_session():
     with Session(db_engine) as session:
         yield session
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(db_engine)
-
-class USER_MODE(Enum):
-    MINIMAL = 0
-    MAXIMAL = 1
