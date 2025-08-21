4dfaa770ae613d456221de566bb560fcd036174d
Andreas Lubojanski <andreas@lubojanski.com>
2025-08-21 11:16:07 +0200

    (update) updated npm package.json

---
+++ backend/app/tests/test_oep_locally.py
@@ -0,0 +1,11 @@
+import pytest
+
+from .test_fixtures import client
+
+@pytest.mark.order(18)
+def test_oep_schemas(client):
+    response = client.get("/oep/local_schemas/sinks")
+    response_data = response.json()
+
+    assert response.status_code == 200
+    assert response_data["success"] == True
