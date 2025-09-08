0470a691d6f2ee1869ecc74ee8cabac55f1f747b
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2025-09-08 20:45:30 +0200

    style(index): footer+index postion img

--- frontend/src/app/core/layout/index/index.component.ts
+++ frontend/src/app/core/layout/index/index.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
     selector: 'app-index',
-    imports: [FooterComponent],
+    imports: [CommonModule, FooterComponent],
     templateUrl: './index.component.html',
     styleUrl: './index.component.scss',
 })
