e7787b297a7df4ae586bedd189f13a5396c7b91e
Andreas Lubojanski <andreas@lubojanski.com>
2025-09-25 16:25:54 +0200

    [updated] fixed automatic forwarding to project page

--- frontend/src/app/core/layout/index/index.component.ts
+++ frontend/src/app/core/layout/index/index.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import {AuthCoreService} from '../../auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-index',
@@ -8,4 +10,18 @@ import { FooterComponent } from '../footer/footer.component';
     templateUrl: './index.component.html',
     styleUrl: './index.component.scss',
 })
-export class IndexComponent {}
+export class IndexComponent implements OnInit{
+
+    authCoreService = inject(AuthCoreService);
+    router = inject(Router);
+
+    ngOnInit() {
+        this.authCoreService.currentToken.subscribe((res) => {
+            if (res) {
+                this.router.navigate(['/projects'])
+            } else {
+                this.router.navigate(['/'])
+            }
+        });
+    }
+}
