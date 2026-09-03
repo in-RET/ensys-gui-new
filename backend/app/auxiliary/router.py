43fd612d0fa6714545018bc0ef8438cf5d313eaf
Andreas Lubojanski <andreas@lubojanski.com>
2026-09-03 11:27:59 +0200

    [update #324] removed unnecessary responses and added token request to the api

--- backend/app/auxiliary/router.py
+++ backend/app/auxiliary/router.py
@@ -1,9 +1,12 @@
 """
 This module defines the router for auxiliary endpoints in the FastAPI application. It includes the necessary imports and sets up the router with a specified prefix and tags for organization.
 """
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Depends
+from starlette import status
 
+from security import oauth2_scheme
 from .service import calculate_ep_costs
 from ..models.base import GeneralDataModel
 from ..models.response import DataResponse
@@ -16,6 +19,7 @@ auxiliary_router = APIRouter(
 
 @auxiliary_router.get("/calculate_ep_costs")
 async def calculate_ep_costs_endpoint(
+    token: Annotated[str, Depends(oauth2_scheme)],
     capex: float,
     opex: float,
     interest_rate: float,
@@ -28,6 +32,11 @@ async def calculate_ep_costs_endpoint(
     - returns: DataResponse with calculated EP costs
     """
 
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated."
+        )
+
     ep_costs = calculate_ep_costs(
         capex=capex,
         opex=opex,
@@ -37,14 +46,7 @@ async def calculate_ep_costs_endpoint(
     )
 
     result_dict = {
-        "ep_costs": ep_costs,
-        "calculation": {
-            "capex": capex,
-            "opex": opex,
-            "interest_rate": interest_rate,
-            "lifetime": lifetime,
-            "deprecation": deprecation,
-        }
+        "ep_costs": ep_costs
     }
 
     return DataResponse(
